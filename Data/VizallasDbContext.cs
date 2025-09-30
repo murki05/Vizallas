4b46a96906847df5adad55c5d7c356589e54a1ac
21bal <21bal@MARK-LENOVO>
2025-09-30 12:48:48 +0200

    Add project files.

---
+++ Data/VizallasDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Vizallas.Data
+{
+    public class VizallasDbContext : DbContext
+    {
+        public VizallasDbContext(DbContextOptions<VizallasDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Model.Class> Classes { get; set; }
+    }
+}
---
+++ Model/Class.cs
@@ -0,0 +1,11 @@
+﻿namespace Vizallas.Model
+{
+    public class Class
+    {
+        public int Id { get; set; }
+        public DateOnly datum { get; set; }
+        public int vizallas { get; set; }
+        public string varos { get; set; }
+        public string folyo { get; set; }
+    }
+}
